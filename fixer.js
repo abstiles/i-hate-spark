aa456d8b78650326bc126c7037d4223cd2ed2de9
Andrew Stiles <andrewbstiles@gmail.com>
2018-03-27 13:24:55 -0500

    Initial support for color code and correct width

---
+++ fixer.js
@@ -0,0 +1,30 @@
+setInterval(Prism.highlightAll, 2000);
+// hljs.initHighlightingOnLoad();
+// setTimeout(function() {
+//     Array.prototype.forEach.call(
+//       document.querySelectorAll('pre code'),
+//       hljs.highlightBlock);
+//     // setInterval(function() {
+//     //   Array.prototype.forEach.call(
+//     //     document.querySelectorAll('pre code:not(.hljs)'),
+//     //     hljs.highlightBlock);
+//     // }, 2000);
+// }, 10000);
+// // var codeBlockObserver = new MutationObserver(function(mutations, obs) {
+// //   for(var i = 0; i < mutations.length; ++i) {
+// //     for(var j = 0; j < mutations[i].addedNodes.length; ++j) {
+// //       var newTag = mutations[i].addedNodes[j];
+// //       console.log(newTag);
+// //       Array.prototype.forEach.call(
+// //         newTag.querySelectorAll('pre code'),
+// //         hljs.highlightBlock);
+// //     }
+// //   }
+// // });
+// // Array.prototype.forEach.call(
+// //     document.documentElement.querySelectorAll('pre code'),
+// //     hljs.highlightBlock);
+// // codeBlockObserver.observe(document.documentElement, {
+// //   childList: true,
+// //   subtree: true
+// // });
