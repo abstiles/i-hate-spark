1be114e95d55270ebe9e48fd36d3207884fd664e
Andrew Stiles <andrewbstiles@gmail.com>
2018-03-27 22:11:13 -0500

    Automatically catch new code blocks

--- fixer.js
+++ fixer.js
@@ -1,30 +1,34 @@
-setInterval(Prism.highlightAll, 2000);
-// hljs.initHighlightingOnLoad();
-// setTimeout(function() {
-//     Array.prototype.forEach.call(
-//       document.querySelectorAll('pre code'),
-//       hljs.highlightBlock);
-//     // setInterval(function() {
-//     //   Array.prototype.forEach.call(
-//     //     document.querySelectorAll('pre code:not(.hljs)'),
-//     //     hljs.highlightBlock);
-//     // }, 2000);
-// }, 10000);
-// // var codeBlockObserver = new MutationObserver(function(mutations, obs) {
-// //   for(var i = 0; i < mutations.length; ++i) {
-// //     for(var j = 0; j < mutations[i].addedNodes.length; ++j) {
-// //       var newTag = mutations[i].addedNodes[j];
-// //       console.log(newTag);
-// //       Array.prototype.forEach.call(
-// //         newTag.querySelectorAll('pre code'),
-// //         hljs.highlightBlock);
-// //     }
-// //   }
-// // });
-// // Array.prototype.forEach.call(
-// //     document.documentElement.querySelectorAll('pre code'),
-// //     hljs.highlightBlock);
-// // codeBlockObserver.observe(document.documentElement, {
-// //   childList: true,
-// //   subtree: true
-// // });
+//setInterval(Prism.highlightAll, 2000);
+
+var codeBlockObserver = new MutationObserver(function(mutations, obs) {
+  for(var i = 0; i < mutations.length; ++i) {
+    if (mutations[i].target.tagName == "CODE") {
+      if (!mutations[i].target.querySelector('.token')) {
+        Prism.highlightAllUnder(mutations[i].target.parentNode);
+      }
+    }
+    for(var j = 0; j < mutations[i].addedNodes.length; ++j) {
+      var newTag = mutations[i].addedNodes[j];
+      if (newTag.querySelector && newTag.querySelector('pre')) {
+        Prism.highlightAllUnder(newTag);
+      }
+    }
+  }
+});
+
+function waitForElementToDisplay(selector, time) {
+  var element = document.querySelector(selector);
+  if (element != null) {
+    Prism.highlightAllUnder(element);
+    codeBlockObserver.observe(element, {
+      childList: true,
+      subtree: true
+    });
+  } else {
+    setTimeout(function() {
+      waitForElementToDisplay(selector, time);
+    }, time);
+  }
+}
+
+waitForElementToDisplay('#activity-items', 1000);
